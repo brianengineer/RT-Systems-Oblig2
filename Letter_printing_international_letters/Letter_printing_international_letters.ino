b60504a8858f3e8efc8207a0a396460a8cdefaca
sleeplessnights <126039@gmail.com>
2013-10-21 11:08:46 +0200

    working morse - converter

---
+++ Letter_printing_international_letters/Letter_printing_international_letters.ino
@@ -0,0 +1,170 @@
+/*
+ *  Dot and Dash Separation Program
+ */
+
+// Parameters
+int HOLD_DELAY = 500;    // Sets the hold delay for the signal to be interpreted as a dash
+int CLICK_DELAY = 20;    // singal minimum length to be a dot - avoid debounce
+int switchPin  = 3;      // Switch is connected to pin 3
+ 
+// In-Program Variables 
+unsigned long start_hold;
+boolean allow = false;
+boolean pressed = false;
+int sw_state;            // nextstate system
+int sw_laststate = LOW;
+int counter = 0;
+int signals[4] = {0,0,0,0};
+
+
+
+unsigned long start;
+unsigned long passed_time;
+
+// Setup
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(switchPin, INPUT);    // Set the switch pin as input
+}
+ 
+// Loop
+void loop()
+{
+    sw_state = digitalRead(switchPin);             // read input value
+
+    dotOrDash();
+
+
+} // end void loop
+
+// store morse signals in array signal
+void setSignals(int count, int value )
+{
+  signals[count-1] = value;
+} // end setSignal
+
+
+
+
+
+// function returns a dot or dash value
+void dotOrDash()
+{
+  
+  if (sw_state == HIGH && sw_laststate == LOW)
+  {  // for button pressing
+    start_hold = millis();                       // mark the time
+    allow = true;    // allow LED state changes
+    pressed = true;
+  } // end if
+  
+  // dash
+  if( allow == true && sw_state == HIGH && sw_laststate == HIGH)
+  {  // if button remains pressed
+      if ((millis() - start_hold) >= HOLD_DELAY)
+      {                   // for longer than x/1000 sec(s)
+         allow = false;         // prevent multiple state changes
+         //Serial.println("Dash");
+         counter++;
+         setSignals(counter, 2);
+         //Serial.println(counter);
+         start = millis();
+
+      } // end inner if
+  } // end outer if
+  
+  // dot
+  if( allow == true && sw_state == LOW && sw_laststate == HIGH)
+  {
+    
+    if( (millis() - start_hold) < HOLD_DELAY && ((millis()-start_hold) > CLICK_DELAY))
+    {
+      allow = false;
+      //Serial.println("Dot");
+      counter++;
+      setSignals(counter, 1);
+      //Serial.println(counter);
+      start = millis();
+    }
+  } // end outer if
+  
+  if( sw_state == HIGH && sw_laststate == HIGH)
+  {
+    start = millis();
+  }
+  
+
+  
+   // reset data and call function interpret to display character equivalent to morse code
+   if( pressed == true && sw_state == LOW && sw_laststate == LOW)
+   {
+     passed_time = millis()-start;
+     if(passed_time > 5000)
+     {
+       pressed = false;
+       //Serial.println("5 seconds passed");
+       displayChar(counter);
+       counter = 0;
+     } // end inner if
+   } // end if
+
+  sw_laststate = sw_state;
+} // end dotOrDash
+
+// display char equal to signs input
+void displayChar(int count)
+{
+  switch (count)
+  {
+    case 1:
+      if(signals[0] == 1)
+        Serial.print("E");
+      else if(signals[0]==2)
+        Serial.print("T");
+      break;
+    case 2:
+      if(signals[0]== 1 && signals[1] == 2)
+        Serial.print("A");
+      else if( signals[0] ==1 && signals[1] == 1)
+        Serial.print("I");
+      else if( signals[0] == 2 && signals[1] == 2)
+        Serial.print("M");
+      else if(signals[0] == 2 && signals[1] == 1)
+        Serial.print("N");
+      break;
+    case 3:
+      if(signals[0]==2 && signals[1]==1 && signals[2]==1)
+        Serial.print("D");
+      else if(signals[0]==2 && signals[1]==2 && signals[2]==1)
+        Serial.print("G");
+      else if(signals[0]==2 && signals[1]==1 && signals[2]==2)
+        Serial.print("K");
+      else if(signals[0]==2 && signals[1]==2 && signals[2]==2)
+        Serial.print("O");
+      else if(signals[0]==1 && signals[1]==2 && signals[2]==1)
+        Serial.print("R");
+      else if(signals[0]==1 && signals[1]==1 && signals[2]==1)
+        Serial.print("S");
+      else if(signals[0]==1 && signals[1]==1 && signals[2]==2)
+        Serial.print("U");
+      break;
+    case 4:
+      if(signals[0]==2 && signals[1]==
+      break;
+    default:
+      break;
+        Serial.print("Invalid Combination Found!");
+  } // end switch
+  
+  for(int i = 0; i < 4; i++)
+  {
+    signals[i] = 0;
+  } // end for loop
+} // end display char    
+  
+  
+  
+  
+  
+  
---
+++ counter_21_10_13/counter_21_10_13.ino
@@ -0,0 +1,101 @@
+/*
+ *  Dot and Dash Separation Program
+ */
+
+// Parameters
+int HOLD_DELAY = 500;    // Sets the hold delay for the signal to be interpreted as a dash
+int CLICK_DELAY = 20;    // singal minimum length to be a dot - avoid debounce
+int switchPin  = 3;      // Switch is connected to pin 3
+ 
+// In-Program Variables 
+unsigned long start_hold;
+boolean allow = false;
+int sw_state;            // nextstate system
+int sw_laststate = LOW;
+int counter = 0;
+
+int signal;
+
+unsigned long start;
+unsigned long passed_time;
+
+// Setup
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(switchPin, INPUT);    // Set the switch pin as input
+}
+ 
+// Loop
+void loop()
+{
+    sw_state = digitalRead(switchPin);             // read input value
+
+    dotOrDash();
+
+
+} // end void loop
+
+
+
+
+
+
+// function returns a dot or dash value
+void dotOrDash()
+{
+  
+  if (sw_state == HIGH && sw_laststate == LOW)
+  {  // for button pressing
+    start_hold = millis();                       // mark the time
+    allow = true;                                // allow LED state changes
+  } // end if
+  
+  // dash
+  if( allow == true && sw_state == HIGH && sw_laststate == HIGH)
+  {  // if button remains pressed
+      if ((millis() - start_hold) >= HOLD_DELAY)
+      {                   // for longer than x/1000 sec(s)
+         allow = false;         // prevent multiple state changes
+         Serial.println("Dash");
+         counter++;
+         Serial.println(counter);
+         start = millis();
+
+      } // end inner if
+  } // end outer if
+  
+  // dot
+  if( allow == true && sw_state == LOW && sw_laststate == HIGH)
+  {
+    
+    if( (millis() - start_hold) < HOLD_DELAY && ((millis()-start_hold) > CLICK_DELAY))
+    {
+      allow = false;
+      Serial.println("Dot");
+      counter++;
+      Serial.println(counter);
+      start = millis();
+    }
+  } // end outer if
+  
+   if( allow == false && sw_state == LOW && sw_laststate == HIGH)
+   {
+     passed_time = millis()-start;
+     if(passed_time > 5000)
+     {
+       Serial.println("5 seconds passed");
+     }
+   }
+     
+
+  
+  
+  sw_laststate = sw_state;
+  
+  
+  
+  
+  
+} // end dotOrDash
+
